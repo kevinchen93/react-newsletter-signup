f974cbccbade27fcd00c1c1d77aa8f2bfa202a46
Kevin Chen <xkevinchen@gmail.com>
2019-02-17 18:40:29 -0500

    test

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           query: {
-            presets: ['@babel/env', '@babel/react']
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         },
       }
